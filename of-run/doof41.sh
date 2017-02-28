b31288011a470779b423ac099c8cb11c97faed43
lmaiztegi001 <lmaiztegi001@ikasle.ehu.es>
2017-02-28 07:00:11 +0100

    change doof file to run 2 cases on travis

--- of-run/doof41.sh
+++ of-run/doof41.sh
@@ -4,3 +4,7 @@
 cd /geawaves/canal2D/canal2DsRAS_sample/
 ./Allrun | grep '^Time = '
 cat postProcessing/probes/0/*
+
+cd /geawaves/canal3D/canal3DsRAS_probes/
+./Allrun | grep '^Time = '
+cat postProcessing/probes/0/*
