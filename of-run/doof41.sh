0b57473395bc1e8eaf8884bb3a50940cc2a8c5e0
lmaiztegi001 <lmaiztegi001@ikasle.ehu.es>
2017-02-28 07:58:14 +0100

    only run one case not to exceded 4MB of log

--- of-run/doof41.sh
+++ of-run/doof41.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 . ~/.bashrc
-cd /geawaves/canal2D/canal2DsRAS_sample/
-./Allrun | grep '^Time = '
-cat postProcessing/probes/0/*
-
 cd /geawaves/canal3D/canal3DsRAS_probes/
 ./Allrun | grep '^Time = '
 cat postProcessing/probes/0/*
