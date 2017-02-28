4adf9dc2b284670ec6613ee37532e858b9239c97
lmaiztegi001 <lmaiztegi001@ikasle.ehu.es>
2017-02-28 05:46:29 +0100

    Without Allrun

--- of-run/doof41.sh
+++ of-run/doof41.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-. ~/.bashrc && cd /geawaves/canal2D/canal2DsRAS_sample/ && ls && ./Allrun && cat postProcessing/probes/0/*
+. ~/.bashrc
+cd /geawaves/canal2D/canal2DsRAS_sample/
+blockMesh 1>> log.log 2>&1
+setFields 1>> log.log 2>&1
+interFoam 1>> log.log 2>&1
+cat postProcessing/probes/0/*
