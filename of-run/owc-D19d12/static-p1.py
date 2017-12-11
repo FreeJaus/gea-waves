aa32e54a9171ece55fef6c02c6b991b9dd83fc02
lmaiztegi001 <lmaiztegi001@ikasle.ehu.es>
2017-12-11 10:20:33 +0100

    python script to plot p

--- of-run/owc-D19d12/static-p1.py
+++ of-run/owc-D19d12/static-p1.py
@@ -21,7 +21,7 @@ animationScene1 = GetAnimationScene()
 # update animation scene based on data timesteps
 animationScene1.UpdateAnimationUsingDataTimeSteps()
 
-animationScene1.GoToNext()
+#animationScene1.GoToNext()
 
 # get active view
 renderView1 = GetActiveViewOrCreate('RenderView')
@@ -83,20 +83,16 @@ view = GetActiveView()
 f=open(path+output_file_name,"w")
 f.write("t,static_Pressure\n")
 for t in time_steps:
-	view.ViewTime = t	
-	Render()
-	calculator1.UpdatePipeline() # Perform the calculation
-	p = calculator1.GetCellData().GetArray('p').GetValue(t)
-	#Pressure_Var_Fetch = servermanager.Fetch(calculator1)
-	#Pressure_Var_Pointdata = Pressure_Var_Fetch.GetPointData()
-	#p1 = Pressure_Var_Pointdata.GetArray('p')
-	#Integrate_Var_Celldata = Integrate_Var_Fetch.GetCellData()
-	#mArea= Integrate_Var_Celldata.GetArray('Area')
-	#Area = mArea.GetValue(0)
-	#p = p1.GetValue(0)
-	f.write("{0},{1}\n".format(t,p)) #ylevel/Area))
-	print "{0},{1}".format(t,p) #ylevel/Area)
-	
+    view.ViewTime = t
+    Render()
+    calculator1.UpdatePipeline() # Perform the calculation
+    p = servermanager.Fetch(calculator1).GetPointData().GetArray('p')
+    if p is not None:
+        p_val = p.GetValue(0)	
+        f.write("{0},{1}\n".format(t,p_val))
+        print "{0},{1}".format(t,p_val)
+    else:
+        print "p has no value"
 f.close()
 
 # create a new 'CSV Reader'
