23a9b904033841ccc5d209803cd5c3adb8dc2560
lmaiztegi001 <lmaiztegi001@ikasle.ehu.es>
2018-06-29 23:47:43 +0200

    memoria estable

---
+++ _hugo/content/tfg/cfd/run_docker.sh
@@ -0,0 +1,1 @@
+docker run --rm -itv $(pwd):/src -w /src btdi/latex bash -c ./make.sh
--- _hugo/content/tfg/make.sh
+++ _hugo/content/tfg/make.sh
@@ -3,15 +3,20 @@
 #rm log.log main.log
 mkdir -pv build
 
+pdflatex -output-directory=build/ -synctex=1 -interaction=nonstopmode main.tex
 printf "> pdflatex [first]\n\n$(pdflatex -output-directory=build/ -synctex=1 -interaction=nonstopmode main.tex)" >> log.log 2> /dev/null
 
 cd build/
+biber main
 printf "\n\n> biber\n\n$(biber main)" >> ../log.log 2> /dev/null
 cd ..
 
-printf "\n\n> pdflatex\n\n$(pdflatex -output-directory=build/ -synctex=1 -interaction=nonstopmode main.tex)" >> log.log 2> /dev/null
-printf "\n\n> pdflatex [last]\n\n$(pdflatex -output-directory=build/ -synctex=1 -interaction=nonstopmode main.tex)" >> log.log 2> /dev/null
-printf "\n\n> pdflatex [last]\n\n$(pdflatex -output-directory=build/ -synctex=1 -interaction=nonstopmode main.tex)" >> log.log 2> /dev/null
+pdflatex -output-directory=build/ -synctex=1 -interaction=nonstopmode main.tex
+pdflatex -output-directory=build/ -synctex=1 -interaction=nonstopmode main.tex
+pdflatex -output-directory=build/ -synctex=1 -interaction=nonstopmode main.tex
+#printf "\n\n> pdflatex\n\n$(pdflatex -output-directory=build/ -synctex=1 -interaction=nonstopmode main.tex)" >> log.log 2> /dev/null
+#printf "\n\n> pdflatex [last]\n\n$(pdflatex -output-directory=build/ -synctex=1 -interaction=nonstopmode main.tex)" >> log.log 2> /dev/null
+#printf "\n\n> pdflatex [last]\n\n$(pdflatex -output-directory=build/ -synctex=1 -interaction=nonstopmode main.tex)" >> log.log 2> /dev/null
 
 #cat build/main.log >> main.log
 
